d2191f07baab2cc7843275e404af973f202883b2
Raghava-git21 <prajaraghava@gmail.com>
2026-05-25 15:26:22 +0530

    third commit

--- React appl/my-first-app/src/App.jsx
+++ React appl/my-first-app/src/App.jsx
@@ -17,16 +17,16 @@ function App() {
       address: address,
       salary: salary
     }
-    let response = await axios.post('http://https://empmngmt.onrender.com:8080/emp/add', empDetails);
+    let response = await axios.post('https://empmngmt.onrender.com:8080/emp/add', empDetails);
     alert("Details sent successfully");
   }
   async function getAllUsers() {
-    let response = await axios.get('http://https://empmngmt.onrender.com:8080/emp/allUsers')
+    let response = await axios.get('https://empmngmt.onrender.com:8080/emp/allUsers')
 
     setUser(response.data);
   }
   async function deleteUserBYId(id) {
-    let response = await axios.delete(`http://https://empmngmt.onrender.com:8080/emp/delete/${id}`)
+    let response = await axios.delete(`https://empmngmt.onrender.com:8080/emp/delete/${id}`)
     getAllUsers();
     alert("User deleted successfully")
 
